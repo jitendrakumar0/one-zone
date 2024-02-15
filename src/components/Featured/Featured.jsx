789f2a2452b1c385b96a93b91251eb82e7a535c7
Jitendra Kumar Bijarnia <70063345+jitendrakumar0@users.noreply.github.com>
2024-02-15 12:48:10 +0530

    jk

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -84,11 +84,11 @@ const Featured = () => {
                                             </ul>
                                         </div>
                                         <div className="w-full shrink-0 flex">
-                                            <div className="w-full grow">
+                                            <div className="grow">
                                                 <div className="w-full md:text-lg font-bold text-black">₹31,990</div>
                                                 <div className="w-full text-gray-600 font-semibold md:text-sm text-xs">₹49,990 <span className="text-lime-600 pl-1">36% off</span></div>
                                             </div>
-                                            <div className="w-full">
+                                            <div className="shrink-0">
                                                 <Link className="text-black w-full bg-theme1 border-2 border-theme1 border-b-black font-bold text-xs uppercase px-4 md:py-2 py-1 text-center inline-flex items-center justify-center gap-2 duration-300 md:hover:bg-black md:hover:text-theme1 md:hover:border-theme1 shadow-md shadow-black/40">Chat with Seller <RiChat1Line className='size-5' /></Link>
                                             </div>
                                             <div className='text-xs font-bold bg-theme1 px-4 py-1 rounded absolute top-4 left-4'>FEATURED</div>
@@ -117,11 +117,11 @@ const Featured = () => {
                                             </ul>
                                         </div>
                                         <div className="w-full shrink-0 flex">
-                                            <div className="w-full grow">
+                                            <div className="grow">
                                                 <div className="w-full md:text-lg font-bold text-black">₹31,990</div>
                                                 <div className="w-full text-gray-600 font-semibold md:text-sm text-xs">₹49,990 <span className="text-lime-600 pl-1">36% off</span></div>
                                             </div>
-                                            <div className="w-full">
+                                            <div className="shrink-0">
                                                 <Link className="text-black w-full bg-theme1 border-2 border-theme1 border-b-black font-bold text-xs uppercase px-4 md:py-2 py-1 text-center inline-flex items-center justify-center gap-2 duration-300 md:hover:bg-black md:hover:text-theme1 md:hover:border-theme1 shadow-md shadow-black/40">Chat with Seller <RiChat1Line className='size-5' /></Link>
                                             </div>
                                             <div className='text-xs font-bold bg-theme1 px-4 py-1 rounded absolute top-4 left-4'>FEATURED</div>
@@ -150,11 +150,11 @@ const Featured = () => {
                                             </ul>
                                         </div>
                                         <div className="w-full shrink-0 flex">
-                                            <div className="w-full grow">
+                                            <div className="grow">
                                                 <div className="w-full md:text-lg font-bold text-black">₹31,990</div>
                                                 <div className="w-full text-gray-600 font-semibold md:text-sm text-xs">₹49,990 <span className="text-lime-600 pl-1">36% off</span></div>
                                             </div>
-                                            <div className="w-full">
+                                            <div className="shrink-0">
                                                 <Link className="text-black w-full bg-theme1 border-2 border-theme1 border-b-black font-bold text-xs uppercase px-4 md:py-2 py-1 text-center inline-flex items-center justify-center gap-2 duration-300 md:hover:bg-black md:hover:text-theme1 md:hover:border-theme1 shadow-md shadow-black/40">Chat with Seller <RiChat1Line className='size-5' /></Link>
                                             </div>
                                             <div className='text-xs font-bold bg-theme1 px-4 py-1 rounded absolute top-4 left-4'>FEATURED</div>
@@ -183,11 +183,11 @@ const Featured = () => {
                                             </ul>
                                         </div>
                                         <div className="w-full shrink-0 flex">
-                                            <div className="w-full grow">
+                                            <div className="grow">
                                                 <div className="w-full md:text-lg font-bold text-black">₹31,990</div>
                                                 <div className="w-full text-gray-600 font-semibold md:text-sm text-xs">₹49,990 <span className="text-lime-600 pl-1">36% off</span></div>
                                             </div>
-                                            <div className="w-full">
+                                            <div className="shrink-0">
                                                 <Link className="text-black w-full bg-theme1 border-2 border-theme1 border-b-black font-bold text-xs uppercase px-4 md:py-2 py-1 text-center inline-flex items-center justify-center gap-2 duration-300 md:hover:bg-black md:hover:text-theme1 md:hover:border-theme1 shadow-md shadow-black/40">Chat with Seller <RiChat1Line className='size-5' /></Link>
                                             </div>
                                             <div className='text-xs font-bold bg-theme1 px-4 py-1 rounded absolute top-4 left-4'>FEATURED</div>
@@ -216,11 +216,11 @@ const Featured = () => {
                                             </ul>
                                         </div>
                                         <div className="w-full shrink-0 flex">
-                                            <div className="w-full grow">
+                                            <div className="grow">
                                                 <div className="w-full md:text-lg font-bold text-black">₹31,990</div>
                                                 <div className="w-full text-gray-600 font-semibold md:text-sm text-xs">₹49,990 <span className="text-lime-600 pl-1">36% off</span></div>
                                             </div>
-                                            <div className="w-full">
+                                            <div className="shrink-0">
                                                 <Link className="text-black w-full bg-theme1 border-2 border-theme1 border-b-black font-bold text-xs uppercase px-4 md:py-2 py-1 text-center inline-flex items-center justify-center gap-2 duration-300 md:hover:bg-black md:hover:text-theme1 md:hover:border-theme1 shadow-md shadow-black/40">Chat with Seller <RiChat1Line className='size-5' /></Link>
                                             </div>
                                             <div className='text-xs font-bold bg-theme1 px-4 py-1 rounded absolute top-4 left-4'>FEATURED</div>
--- src/components/SellingProducts/SellingProducts.jsx
+++ src/components/SellingProducts/SellingProducts.jsx
@@ -135,11 +135,11 @@ const SellingProducts = () => {
                                             </ul>
                                         </div>
                                         <div className="w-full shrink-0 flex">
-                                            <div className="w-full grow">
+                                            <div className="grow">
                                                 <div className="w-full md:text-lg font-bold text-black">₹31,990</div>
                                                 <div className="w-full text-gray-600 font-semibold md:text-sm text-xs">₹49,990 <span className="text-lime-600 pl-1">36% off</span></div>
                                             </div>
-                                            <div className="w-full">
+                                            <div className="shrink-0">
                                                 <Link className="text-black w-full bg-theme1 border-2 border-theme1 border-b-black font-bold text-xs uppercase md:px-6 px-4 md:py-2 py-1 text-center inline-flex items-center justify-center gap-2 duration-300 md:hover:bg-black md:hover:text-theme1 md:hover:border-theme1 shadow-md shadow-black/40">Chat with Seller <RiChat1Line className='size-5' /></Link>
                                             </div>
                                         </div>
@@ -167,11 +167,11 @@ const SellingProducts = () => {
                                             </ul>
                                         </div>
                                         <div className="w-full shrink-0 flex">
-                                            <div className="w-full grow">
+                                            <div className="grow">
                                                 <div className="w-full md:text-lg font-bold text-black">₹31,990</div>
                                                 <div className="w-full text-gray-600 font-semibold md:text-sm text-xs">₹49,990 <span className="text-lime-600 pl-1">36% off</span></div>
                                             </div>
-                                            <div className="w-full">
+                                            <div className="shrink-0">
                                                 <Link className="text-black w-full bg-theme1 border-2 border-theme1 border-b-black font-bold text-xs uppercase md:px-6 px-4 md:py-2 py-1 text-center inline-flex items-center justify-center gap-2 duration-300 md:hover:bg-black md:hover:text-theme1 md:hover:border-theme1 shadow-md shadow-black/40">Chat with Seller <RiChat1Line className='size-5' /></Link>
                                             </div>
                                         </div>
@@ -199,11 +199,11 @@ const SellingProducts = () => {
                                             </ul>
                                         </div>
                                         <div className="w-full shrink-0 flex">
-                                            <div className="w-full grow">
+                                            <div className="grow">
                                                 <div className="w-full md:text-lg font-bold text-black">₹31,990</div>
                                                 <div className="w-full text-gray-600 font-semibold md:text-sm text-xs">₹49,990 <span className="text-lime-600 pl-1">36% off</span></div>
                                             </div>
-                                            <div className="w-full">
+                                            <div className="shrink-0">
                                                 <Link className="text-black w-full bg-theme1 border-2 border-theme1 border-b-black font-bold text-xs uppercase md:px-6 px-4 md:py-2 py-1 text-center inline-flex items-center justify-center gap-2 duration-300 md:hover:bg-black md:hover:text-theme1 md:hover:border-theme1 shadow-md shadow-black/40">Chat with Seller <RiChat1Line className='size-5' /></Link>
                                             </div>
                                         </div>
@@ -231,11 +231,11 @@ const SellingProducts = () => {
                                             </ul>
                                         </div>
                                         <div className="w-full shrink-0 flex">
-                                            <div className="w-full grow">
+                                            <div className="grow">
                                                 <div className="w-full md:text-lg font-bold text-black">₹31,990</div>
                                                 <div className="w-full text-gray-600 font-semibold md:text-sm text-xs">₹49,990 <span className="text-lime-600 pl-1">36% off</span></div>
                                             </div>
-                                            <div className="w-full">
+                                            <div className="shrink-0">
                                                 <Link className="text-black w-full bg-theme1 border-2 border-theme1 border-b-black font-bold text-xs uppercase md:px-6 px-4 md:py-2 py-1 text-center inline-flex items-center justify-center gap-2 duration-300 md:hover:bg-black md:hover:text-theme1 md:hover:border-theme1 shadow-md shadow-black/40">Chat with Seller <RiChat1Line className='size-5' /></Link>
                                             </div>
                                         </div>
